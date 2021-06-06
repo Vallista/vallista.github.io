44ece92ef1a0b0a9a4e67d047186ea2c9b4366ad
마광휘 <mgh950714@gmail.com>
2021-06-07 00:54:40 +0900

    Updates

---
+++ component---src-pages-404-tsx-d626980493df261aefc8.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{9786:function(t,e,a){"use strict";a.r(e);var u=a(7294);e.default=function(){return u.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-d626980493df261aefc8.js.map
