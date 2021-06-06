1b2a7cd11b383ab47150cdc5274ab8557074b073
마광휘 <mgh950714@gmail.com>
2021-06-07 02:07:16 +0900

    feat: google analytics 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,25 @@ module.exports = {
     twitterUsername: ""
   },
   plugins: [{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-143764638-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "vallista.kr",
+      },
+    }, {
       resolve: `gatsby-plugin-disqus`,
       options: {
         shortname: `vallista-1`
