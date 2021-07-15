157bfac4deb84ec5539fe53a91c836f52e6f7221
마광휘 <mgh950714@gmail.com>
2021-07-15 23:59:53 +0900

    feat: 시리즈 작업 시작

---
+++ src/components/Series/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Series: React.VFC<Props> = (props) => {
+  return (
+    <div>
+      Series!
+    </div>
+  )
+}
+
+export default Series;
