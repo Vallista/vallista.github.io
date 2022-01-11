68180ad41c6a30d7ca23875871fd9791a951e041
마광휘 <mgh950714@gmail.com>
2022-01-11 21:46:40 +0900

    Updates

---
+++ component---src-pages-404-tsx-a4d52d4038857358a4ee.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_vallista_land_blog=self.webpackChunk_vallista_land_blog||[]).push([[218],{4202:function(e,t,n){n.r(t),n.d(t,{default:function(){return f}});var a=n(3919),i=n(3367),l=n.p+"static/failure-c8fee5d12dd160f4be5138d98ae354f0.gif",s=n(2322);var c=(0,a.Z)("div",{target:"e1em2f1n0"})({name:"1vcob1d",styles:"display:flex;justify-content:center;align-items:center"}),f=function(){return(0,s.jsx)(c,{children:(0,s.jsx)(i.Ee,{src:l,width:400,height:400})})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-a4d52d4038857358a4ee.js.map
